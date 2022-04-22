585208babee348dae5e9e5ae75e807497e63438b
Ntsakelo <Ntsaakeloarnold00@gmail.com>
2022-04-22 15:28:22 +0200

    Updated kamvasCattery

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,15 @@
+describe ('test bookings function',function(){
+    it("This booking should 'cost: R45 * 3 = R135.00' and the function should return that if called with this parameter.",function(){
+        assert.equal(135,bookingCost({check_in_day: 'Monday',duration: 3, cat_name : 'Snowy'
+          }))
+    })
+    it("This booking should 'cost: R45 * 3 = R135.00 * 0.75 = 101.25' and the function should return that if called with this parameter.",function(){
+        assert.equal(101.25,bookingCost({check_in_day: 'Wednesday',duration: 3, cat_name : 'Snowy'
+          }))
+    })
+    it("This booking should 'cost: R45 * 3 = R135.00 * 0.85 = 114.75' and the function should return that if called with this parameter.",function(){
+        assert.equal(114.75,bookingCost({check_in_day: 'Friday',duration: 3, cat_name : 'Snowy'
+          }))
+    })
 
+})
--- weekly_wages/weekly_wages.js
+++ weekly_wages/weekly_wages.js
@@ -1,3 +1,27 @@
 function weeklyWage(band, weeklyHours) {
+  let hrsArr = weeklyHours.split(',');
+  let A = 23;
+  let B = 37;
+  let C = 51;
+  let hrsTot = 0;
+  for(let i = 0; i < hrsArr.length;i++){
+      let hrsStr = hrsArr[i].trim();
+      let hrsNum = parseInt(hrsStr);
+          hrsTot += hrsNum;
+     }
+        if(band === 'A' && hrsArr.length === 7){
+           let wage = hrsTot * 23;
+            return wage;
+        }else if(band === 'B' && hrsArr.length === 7){
+            let wage = hrsTot * 37;
+            return wage
+        }else if(band === 'C' && hrsArr.length === 7){
+            let wage = hrsTot * 51;
+            return wage;
+        }else if(hrsArr.length !== 7){
+
+            return 'missing day';
+        }
+        return 0;
 
 };
